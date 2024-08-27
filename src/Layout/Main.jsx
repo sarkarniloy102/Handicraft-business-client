ba1597982abdfb8f541c7449318e09c5896e5a4a
sarkar niloy <sarkarniloy102@gmail.com>
2024-08-27 16:32:18 +0600

    footer part

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,15 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
+import Footer from "../Shared/Footer/Footer";
 
 
 const Main = () => {
     return (
         <div data-theme="light" >
             <div className="mx-4 lg:mx-20">
-            <Navbar></Navbar>
-            <Outlet></Outlet>
+                <Navbar></Navbar>
+                <Outlet></Outlet>
             </div>
-            
+            <Footer></Footer>
+
+
         </div>
     );
 };
